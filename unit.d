33414ba7488b0fa605dcbf8c3ace512d21ffd9ef
KD <gkai70264@gmail.com>
2022-04-28 15:44:55 -0400

    Better doc

--- unit.d
+++ unit.d
@@ -10,12 +10,15 @@ void main()
 {
     auto ms = handy_newstate();
 
-    /* You can change things before and after you parse, if you allow callbacks, you need to create a callback function or else you'll get SEGFAULT. */
+    /* You can change things before and after you parse, if you allow callbacks, 
+    you need to create a callback function or else you'll get SEGFAULT. */
     handy_set_callback(ms, &mycallback);
 
     handy_allow_callback(ms);
 
-    handy_add_excl_char(ms, '\'', handy_charstate.stringlike);
+    handy_add_excl_char(ms, '\'', handy_charstate.stringlike); 
+    /* when you exclude a character, you're basically telling the 
+    parser to either treat it as a string (open and close), or ignore it fully. */
 
     handy_parse_result(ms, "['1,', 2,3]");
 
